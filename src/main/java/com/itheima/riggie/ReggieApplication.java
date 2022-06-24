289869b12b6d29cd23dbf4f77952125d67ef70a1
YczT <ztong0_0@163.com>
2022-06-24 14:55:39 +0800

    ayan提交了代码

--- src/main/java/com/itheima/riggie/ReggieApplication.java
+++ src/main/java/com/itheima/riggie/ReggieApplication.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Slf4j
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class ReggieApplication {
     public static void main(String[] args) {
+//        ConfigurableApplicationContext run =
         SpringApplication.run(ReggieApplication.class, args);
         log.info("项目启动成功...");
     }
